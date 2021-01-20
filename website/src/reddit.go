6c2543acf08b7b3f189c3c8c658444d5b8ad1b19
Fernando Remde <ffremde@gmail.com>
2021-01-20 19:43:58 -0300

    add reddit structure

---
+++ website/src/reddit.go
@@ -0,0 +1,17 @@
+package lobsters
+
+import "github.com/remde/myfeed/website"
+
+type redditTitle struct {
+	Title string `json:"selftext"`
+	URL   string `json:"url"`
+}
+
+func (reddit redditTitle) getTitles(subreddit string) {
+	arrayOfTitles := new([]redditTitle)
+	website.SetTitlesToStruct(arrayOfTitles, getRedditURL(subreddit))
+}
+
+func getRedditURL(subreddit string) string {
+	return "https://reddit.com/r/" + subreddit + "/.json"
+}
