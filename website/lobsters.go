6a228fa191cee647274f69dfd8385deffd0ce54d
Fernando Remde <ffremde@gmail.com>
2021-01-22 20:41:38 -0300

    Add RawRedditStruct to differentiate between the final structure

--- website/lobsters.go
+++ website/lobsters.go
@@ -1,13 +1,28 @@
 package website
 
-//LobsterTitle is the structure for the lobste.rs website
-type LobsterTitle struct {
-	Title string `json:"title"`
-	URL   string `json:"url"`
+//RawLobsterStruct is the structure gotten from lobste.rs website
+type RawLobsterStruct struct {
+	Title         string `json:"title"`
+	URL           string `json:"url"`
+	Score         int    `json:"score"`
+	CommentCount  int    `json:"comment_count"`
+	CommentsURL   string `json:"comments_url"`
+	SubmitterUser struct {
+		Username string `json:"username"`
+	} `json:"submitter_user"`
+}
+
+type lobsterStruct struct {
+	Title        string
+	URL          string
+	Score        int
+	CommentCount int
+	CommentsURL  string
+	SubmiterUser string
 }
 
 //GetLobstersTitles stores titles and URLs to target struct
-func GetLobstersTitles(arrayOfTitles *[]LobsterTitle) {
+func GetLobstersTitles(arrayOfTitles *[]RawLobsterStruct) {
 	lobstersURL := "https://lobste.rs"
 	SetTitlesToStruct(arrayOfTitles, lobstersURL)
 }
