4c5e0cd9e9f35e4a5498af62f07d89738911d36d
Fernando Remde <ffremde@gmail.com>
2021-01-25 18:49:31 -0300

    Merge parser service into cli

--- cli/cli.go
+++ cli/cli.go
@@ -3,17 +3,16 @@ package cli
 import (
 	"fmt"
 
-	"github.com/remde/myfeed/parser"
 	"github.com/remde/myfeed/website"
 )
 
 //Start initializes the CLI app
-func Start(config *parser.Config) {
+func Start(config *config) {
 	initWebsites(config)
 	//reader := bufio.NewReader(os.Stdin)
 }
 
-func initWebsites(config *parser.Config) {
+func initWebsites(config *config) {
 	switch configWebsite := config.Websites[0]; configWebsite {
 	case "lobsters":
 		website.InitLobsters(config.MaxLinks)
--- cli/parser.go
+++ cli/parser.go
@@ -1,15 +1,14 @@
-package parser
+package cli
 
-//Config is the structure for the user's default config
-type Config struct {
+type config struct {
 	Websites []string
 	MaxLinks int
 	Browser  string
 }
 
 //GetInitConfig parses the config.txt file in the root to obtain the user's default config
-func GetInitConfig() *Config {
-	initConfig := new(Config)
+func GetInitConfig() *config {
+	initConfig := new(config)
 	initConfig.Websites = []string{"reddit", "lobsters"}
 	initConfig.MaxLinks = 5
 	initConfig.Browser = "chrome"
