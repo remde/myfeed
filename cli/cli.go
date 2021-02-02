ca5a759ee2c15a9514c97661148eb8a5bb0dbd2f
Fernando Remde <ffremde@gmail.com>
2021-02-02 18:38:45 -0300

    Add choice option to CLI

--- cli/cli.go
+++ cli/cli.go
@@ -8,18 +8,26 @@ import (
 
 //Start initializes the CLI app
 func Start(config *config) {
-	initWebsites(config)
-	//reader := bufio.NewReader(os.Stdin)
+	lobstersTable := initWebsites(config)
+	fmt.Println("Enter number: ")
+	var index int
+	_, err := fmt.Scanf("%d", &index)
+	if err != nil {
+		fmt.Println("error")
+	}
+	fmt.Println(lobstersTable[index])
 }
 
-func initWebsites(config *config) {
+func initWebsites(config *config) website.WebsiteTable {
+	lobstersTable := website.WebsiteTable{}
 	for _, chosenWebsite := range config.Websites {
 		if chosenWebsite == "lobsters" {
-			website.InitLobsters(config.MaxLinks)
+			lobstersTable = website.InitLobsters(config.MaxLinks)
 		} else if chosenWebsite == "reddit" {
 			website.InitReddit(config.MaxLinks)
 		} else {
 			fmt.Println("This website is not yet supported: " + chosenWebsite)
 		}
 	}
+	return lobstersTable
 }
