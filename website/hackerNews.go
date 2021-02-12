c5b6c646e03791f02de2e31041f371212dbcfe52
remde <ffremde@gmail.com>
2021-02-12 14:11:15 -0300

    Comment hacker news as it is not yet functional

--- website/hackerNews.go
+++ website/hackerNews.go
@@ -1,37 +1,37 @@
 package website
 
-import "fmt"
+// import "fmt"
 
-type rawHackerNewsStruct struct {
-}
+// type rawHackerNewsStruct struct {
+// }
 
-type rawHackerNewsStructArr []rawHackerNewsStruct
+// type rawHackerNewsStructArr []rawHackerNewsStruct
 
-//InitHackerNews intializes Hacker News structure and prints the titles to the CLI
-func InitHackerNews(maxLinks int) WebsiteTable {
-	hackerNewsURL := "https://hackernews.com"
-	rawHackerNewsStruct := new(rawHackerNewsStruct)
-	setTitlesToStruct(hackerNewsURL, rawHackerNewsStruct)
-	rawHackerNewsStruct.printTitlesToScreen(maxLinks)
-	hackerNewsStructTable := rawHackerNewsStruct.makeWebsiteStruct(maxLinks)
-	return hackerNewsStructTable
-}
+// //InitHackerNews intializes Hacker News structure and prints the titles to the CLI
+// func InitHackerNews(maxLinks int) WebsiteTable {
+// 	hackerNewsURL := "https://hackernews.com"
+// 	rawHackerNewsStruct := new(rawHackerNewsStruct)
+// 	setTitlesToStruct(hackerNewsURL, rawHackerNewsStruct)
+// 	rawHackerNewsStruct.printTitlesToScreen(maxLinks)
+// 	hackerNewsStructTable := rawHackerNewsStruct.makeWebsiteStruct(maxLinks)
+// 	return hackerNewsStructTable
+// }
 
-func (rawHackerNewsStruct rawHackerNewsStructArr) printTitlesToScreen(maxLinks int) {
-	for i := 0; i <= maxLinks; i++ {
-		fmt.Printf("%d: %s\n", i+1, rawHackerNewsStruct[i].Title)
-	}
-}
+// func (rawHackerNewsStruct rawHackerNewsStructArr) printTitlesToScreen(maxLinks int) {
+// 	for i := 0; i <= maxLinks; i++ {
+// 		fmt.Printf("%d: %s\n", i+1, rawHackerNewsStruct[i].Title)
+// 	}
+// }
 
-func (rawHackerNewsStruct rawHackerNewsStructArr) makeWebsiteStruct(maxLinks int) WebsiteTable {
-	websiteTable := make(WebsiteTable, maxLinks)
-	hackerNewsStruct := websiteStructArr{}
-	for i := 0; i <= maxLinks; i++ {
-		article := WebsiteStruct{}
-		article.URL = rawHackerNewsStruct[i].URL
-		article.CommentsURL = rawHackerNewsStruct[i].CommentsURL
-		hackerNewsStruct = append(hackerNewsStruct, article)
-		websiteTable[i+1] = hackerNewsStruct
-	}
-	return websiteTable
-}
+// func (rawHackerNewsStruct rawHackerNewsStructArr) makeWebsiteStruct(maxLinks int) WebsiteTable {
+// 	websiteTable := make(WebsiteTable, maxLinks)
+// 	hackerNewsStruct := websiteStructArr{}
+// 	for i := 0; i <= maxLinks; i++ {
+// 		article := WebsiteStruct{}
+// 		article.URL = rawHackerNewsStruct[i].URL
+// 		article.CommentsURL = rawHackerNewsStruct[i].CommentsURL
+// 		hackerNewsStruct = append(hackerNewsStruct, article)
+// 		websiteTable[i+1] = hackerNewsStruct
+// 	}
+// 	return websiteTable
+// }
