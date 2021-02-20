790ffc1b2002746bc49c8ea2f15ef15f0111ce0f
remde <ffremde@gmail.com>
2021-02-20 18:33:05 -0300

    Move openArticle to a separate "util.go" file

--- cli/cli.go
+++ cli/cli.go
@@ -2,9 +2,6 @@ package cli
 
 import (
 	"fmt"
-	"log"
-	"os/exec"
-	"runtime"
 
 	"github.com/remde/myfeed/website"
 )
@@ -37,22 +34,4 @@ func initWebsites(config *config) (website.WebsiteTable, string) {
 		}
 	}
 	return lobstersTable, lobstersArticles
-}
-
-func openArticle(url string) {
-	var err error
-
-	switch runtime.GOOS {
-	case "linux":
-		err = exec.Command("xdg-open", url).Start()
-	case "windows":
-		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	case "darwin":
-		err = exec.Command("open", url).Start()
-	default:
-		err = fmt.Errorf("unsupported platform")
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
 }
