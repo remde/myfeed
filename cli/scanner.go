6a574ce5c16f1b563852aa61e2dba20f23b37ddf
remde <ffremde@gmail.com>
2021-02-20 18:32:42 -0300

    Refactor chooseArticleOrComment return

--- cli/scanner.go
+++ cli/scanner.go
@@ -25,7 +25,6 @@ func chooseArticleOrComment(articleURL, articleCommentsURL string) string {
 	}
 	if option == 0 {
 		return articleURL
-	} else {
-		return articleCommentsURL
 	}
+	return articleCommentsURL
 }
